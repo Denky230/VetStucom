025a8dcc1bca81c4ec76416501230a5eb1159a35
Denky <sorek230@gmail.com>
2019-01-30 20:18:34 +0100

    Initial commit

---
+++ src/vetstucom/VetStucom.java
@@ -0,0 +1,12 @@
+
+package vetstucom;
+
+import persistence.DBConnection;
+
+public class VetStucom {
+
+    public static void main(String[] args) {
+
+        DBConnection db = DBConnection.getInstance();
+    }
+}
